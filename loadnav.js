d95383dcdcacc8851a67f7a10341a605361291cb
Jennifer Mackey <jennym1211@hotmail.com>
2021-05-30 00:17:24 -0400

    FirstDraft

---
+++ loadnav.js
@@ -0,0 +1,7 @@
+document.getElementById("headerNav").innerHTML =
+  "<ul id='navbar'><li id='navli'><a href='services.html'>Services</a></li>" +
+  "<li id='navli'><a href='contact.html'>Contact</a></li>" +
+  "<li id='navli'><a href='about.html'>About</a></li>" +
+  " <li id='navli'><a href='book.html'>Book Appointment</a></li></ul>";
+
+document.getElementById("footerNav").innerHTML = "";

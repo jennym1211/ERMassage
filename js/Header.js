a41c41e2276756171155ca171d8bb3afbee1f383
Jennifer Mackey <jennym1211@hotmail.com>
2021-06-09 22:45:16 -0400

    Updates

---
+++ js/Header.js
@@ -0,0 +1,42 @@
+const headerTemplate = document.createElement("template");
+headerTemplate.innerHTML = `
+<link rel="stylesheet" href="css/style.css" />
+
+<div class="navcontainer">
+
+
+<img id="logo" src="images/Escape & Revitalize Massage.png">
+
+
+<div class="navbar">
+    <a href='index.html'>Home</a></li>
+ 
+  <div class="dropdown">
+    <button class="dropbtn"><a href="services.html">Services</a>
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="upgrades.html">Upgrade Options</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div>
+    <a href='contact.html'>Contact</a></li> 
+   <a href='about.html'>About</a></li>
+   <a href='book.html'>Book Appointment</a></li>
+ </div>
+ </div>
+`;
+
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: "closed" });
+    shadowRoot.appendChild(headerTemplate.content);
+  }
+}
+
+customElements.define("header-nav", Header);
--- loadnav.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("headerNav").innerHTML =
-  "<ul id='navbar'><li id='navli'><a href='services.html'>Services</a></li>" +
-  "<li id='navli'><a href='contact.html'>Contact</a></li>" +
-  "<li id='navli'><a href='about.html'>About</a></li>" +
-  " <li id='navli'><a href='book.html'>Book Appointment</a></li></ul>";
-
-document.getElementById("footerNav").innerHTML = "";

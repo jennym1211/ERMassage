60cbb166feba38d0f2a6c515ba5d35dbb9bce848
Jennifer Mackey <jennym1211@hotmail.com>
2021-06-14 04:52:58 -0400

    AboutDone
    Need to add message coding

--- js/Footer.js
+++ js/Footer.js
@@ -2,31 +2,77 @@ const footerTemplate = document.createElement("template");
 footerTemplate.innerHTML = `
 <link rel="stylesheet" href="css/style.css" />
 
-
 <footer>
-<div class="foot-grid">
-<table id="hoo">
 
-  <tr><th>Hours of Operation:</th></tr>
-  <tr><th>Mon & Tues:</th><td>Closed</td></tr>
-  <tr><th>Wednesday:</th><td>11am - 8pm</td></tr>
-  <tr><th>Thursday:</th><td>11am - 8pm</td></tr>
-  <tr><th>Friday:</th><td>1pm - 8pm</td></tr>
-  <tr><th>Saturday:</th><td>11am - 8pm</td></tr>
-  <tr><th>Sunday:</th><td>1pm - 5pm</td></tr>
-  </table>
+    <div class="foot-grid">
+        <table id="hoo">
 
-  <table id="contact">
+            <tr>
+                <th>Hours of Operation:</th>
+            </tr>
+            <tr>
+                <th>Mon & Tues:</th>
+                <td>Closed</td>
+            </tr>
+            <tr>
+                <th>Wednesday:</th>
+                <td>11am - 8pm</td>
+            </tr>
+            <tr>
+                <th>Thursday:</th>
+                <td>11am - 8pm</td>
+            </tr>
+            <tr>
+                <th>Friday:</th>
+                <td>1pm - 8pm</td>
+            </tr>
+            <tr>
+                <th>Saturday:</th>
+                <td>11am - 8pm</td>
+            </tr>
+            <tr>
+                <th>Sunday:</th>
+                <td>1pm - 5pm</td>
+            </tr>
+        </table>
 
-  <tr><th><i class="fas fa-phone"></i> 817-556-1349</th></tr>
-  <tr><td>  Escape & Revitalize Massage</td></tr>
-  <tr><td> 3901 Airport Fwy Ste 312</td></tr>
-  <tr><td>Bedford, TX, 76021</td></tr>  </table>
+        <table id="ftrlinks">
 
-</div>
-<div id="copyright">
-&copy;2021 Escape & Revitalize Massage.
-</div>
+        <tr>
+            <th></th>
+        </tr>
+        <tr>
+            <td> <a href="policy.html">Policies/FAQ</a></td>
+        </tr>
+        <tr>
+            <td> <a href="contact.html">Contact Us</a></td>
+        </tr>
+
+    </table>
+
+
+
+        <table id="contact">
+
+            <tr>
+                <th><i class="fas fa-phone"></i> 817-556-1349</th>
+            </tr>
+            <tr>
+                <td> Escape & Revitalize Massage</td>
+            </tr>
+            <tr>
+                <td> 3901 Airport Fwy Ste 312</td>
+            </tr>
+            <tr>
+                <td>Bedford, TX, 76021</td>
+            </tr>
+        </table>
+
+    </div>
+    <div id="copyright">
+        &copy;2021 Escape & Revitalize Massage.
+
+    </div>
 </footer>
 `;
 
--- js/Header.js
+++ js/Header.js
@@ -1,34 +1,52 @@
+var navbarscript = document.createElement("script");
+navbarscript.src = "js/mobilenavbar.js";
+
 const headerTemplate = document.createElement("template");
 headerTemplate.innerHTML = `
 <link rel="stylesheet" href="css/style.css" />
-
 <div class="navcontainer">
 
+<img id="erlogo" src="images/Escape & Revitalize Massage.png">
 
-<img id="logo" src="images/Escape & Revitalize Massage.png">
 
-<nav class="navbar">
-  <ul class="navlist">
-    <li class="navli"><a href='index.html'>Home</a></li>
-    <li class="navli"><a href="services.html">Services</a>
-      <ul class="dropdown" aria-label="submenu">
-      <li class="innerdrop"><a href="services.html">Services</a></li>
-        <li class="innerdrop"><a href="upgrades.html">Upgrade Options</a></li>
+    <nav class="navbar">
+    <div class="toggle-btn" onclick="toggleSidebar(this)">
+<span></span>
+<span></span>
+<span></span>
+</div>
+        <ul class="navlist">
+            <li class="navli"><a href='index.html'>Home</a></li>
+            <li class="navli"><a href="services.html">Services</a>
+                <ul class="dropdown" aria-label="submenu">
+                    <li class="innerdrop"><a href="services.html">Services</a></li>
+                    <li class="innerdrop"><a href="upgrades.html">Upgrade Options</a></li>
 
-      </ul>
-    </li>
-    <li class="navli">About
-    <ul class="dropdown" aria-label="submenu">
-      <li class="innerdrop"><a href="contact.html">Contact Us</a></li>
-      <li class="innerdrop"><a href="policy.html">Massage Policies</a></li>
+                </ul>
+            </li>
+            <li class="navli">About
+                <ul class="dropdown" aria-label="submenu">
+                    <li class="innerdrop"><a href="contact.html">Contact Us</a></li>
+                    <li class="innerdrop"><a href="policy.html">Policies/FAQ</a></li>
 
-    </ul>
-  </li>
-    <li class="navli"><a href='book.html'>Book Appointment</a></li>
-  </ul>
-</nav>
- </div>
+                </ul>
+            </li>
+            <li class="navli"><a href='book.html'>Book Appointment</a></li>
+        </ul>
+    </nav>
+</div>
+
+
+
+
+
+
+
 `;
+
+headerTemplate.appendChild(navbarscript);
+
+
 
 class Header extends HTMLElement {
   constructor() {

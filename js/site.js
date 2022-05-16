f263b07ec0c402f36ab460d0781157e4e5e099a4
Jennifer Mackey <jennym1211@hotmail.com>
2022-05-15 19:25:11 -0500

    Reverted website back to html/css format, used jquery for framework for reusablity.

---
+++ js/site.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+});
+
+function toggleSidebar(ref) {
+    document.getElementById("sidebar").classList.toggle('active');
+}
